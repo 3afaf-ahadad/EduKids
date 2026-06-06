ff44136734d9f5efb67aa911d378c0f5f679e6a4
Afaf <afaf20ahadad@gmail.com>
2026-06-06 21:50:41 +0000

    feat: adding an alphabet, number and a color model with fillable arrays

---
+++ backend/app/Models/Number.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Number extends Model
+{
+        protected $fillable = [
+        'value',
+        'word',
+        'sound_url',
+    ];
+
+}
