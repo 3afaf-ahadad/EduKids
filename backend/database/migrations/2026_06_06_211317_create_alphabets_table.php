3946dda82401fc0dd0375a3de93c3b34eb98fed8
Afaf <afaf20ahadad@gmail.com>
2026-06-06 21:29:39 +0000

    feat: Adding migrationsfor children, alphabets, numbers and colors

---
+++ backend/database/migrations/2026_06_06_211317_create_alphabets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alphabets', function (Blueprint $table) {
+            $table->id();
+            $table->string('letter_uppercase', 1);
+            $table->string('letter_lowercase', 1);
+            $table->string('sound_url');
+            $table->string('image_url');
+            $table->string('example_word');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alphabets');
+    }
+};
---
+++ backend/database/migrations/2026_06_06_211414_create_numbers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('numbers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('value')->unique();
+            $table->string('word');
+            $table->string('sound_url');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('numbers');
+    }
+};
